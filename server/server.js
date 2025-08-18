8e8ed18c5face3d40a209b879f827793c63275c1
tatang <lintangpsa@gmail.com>
2025-08-18 17:34:37 +0700

    add new origin

--- server/server.js
+++ server/server.js
@@ -18,14 +18,14 @@ await connectDB();
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://greencart-livid-two.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-livid-two.vercel.app', 'https://greencart-2-rho.vercel.app']
 
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 
 app.get("/", (req, res) => {
