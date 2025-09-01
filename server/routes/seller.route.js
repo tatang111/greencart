16753bd2dc2f54ed27ec3cb2cf2f5ee70132d221
tatang <lintangpsa@gmail.com>
2025-09-01 23:33:39 +0700

    fixed bug

--- server/routes/seller.route.js
+++ server/routes/seller.route.js
@@ -6,7 +6,7 @@ const sellerRouter = Router();
 
 sellerRouter.post("/login", sellerLogin);
 sellerRouter.get("/is-auth", authSeller, isSellerAuth);
-sellerRouter.get("/logout", sellerLogout);
+sellerRouter.delete("/logout", authSeller, sellerLogout);
 
 
 export default sellerRouter
